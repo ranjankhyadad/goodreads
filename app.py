643ba775f254122a6d0a443d1281808c3cab09d5
Ranjan Khyadad <ranjankhyadad@gmail.com>
2020-09-20 20:31:06 +0530

    Handle exceptions

--- app.py
+++ app.py
@@ -8,61 +8,64 @@ load_dotenv()
 api_key = os.getenv("API_KEY")
 
 def get_book_details(url):
+    try:
+        resp = ET.fromstring(requests.get(url).content)
 
-    resp = ET.fromstring(requests.get(url).content)
+        # Check if the xml response is valid
+        if ET.iselement(resp):
 
-    # DictSchema-> {  
-    #                   title string
-    #                   average_rating : float
-    #                   ratings_count : int
-    #                   num_pages : int
-    #                   image_url : string
-    #                   publication_year : string
-    #                   authors : string
-    #               }
+            # DictSchema-> {  
+            #                   title string
+            #                   average_rating : float
+            #                   ratings_count : int
+            #                   num_pages : int
+            #                   image_url : string
+            #                   publication_year : string
+            #                   authors : string
+            #               }
 
-    bookDict = {}
-    for element in resp[1]:
-        if element.tag == "title":
-            bookDict["title"] = str(element.text)
+            bookDict = {}
+            for element in resp[1]:
+                if element.tag == "title":
+                    bookDict["title"] = str(element.text)
 
-        if element.tag == "average_rating":
-            bookDict["average_rating"] = float(element.text)
+                if element.tag == "average_rating":
+                    bookDict["average_rating"] = float(element.text)
 
-        if element.tag == "ratings_count":
-            bookDict["ratings_count"] = int(element.text)
+                if element.tag == "ratings_count":
+                    bookDict["ratings_count"] = int(element.text)
+                    
+                if element.tag == "num_pages":
+                    bookDict["num_pages"] = int(element.text)
+        
+                if element.tag == "image_url":
+                    bookDict["image_url"] = str(element.text)
 
-        if element.tag == "num_pages":
-            bookDict["num_pages"] = int(element.text)
- 
-        if element.tag == "image_url":
-            bookDict["image_url"] = str(element.text)
+                if element.tag == "publication_year":
+                    bookDict["publication_year"] = int(element.text)
 
-        if element.tag == "publication_year":
-            bookDict["publication_year"] = int(element.text)
+                if element.tag == "authors":
+                    author_list = []
+                    for author in element:
+                        for child in author:
+                            if child.tag == "name":
+                                author_list.append(child.text)
+                                bookDict["authors"]= (",").join(author_list)
+        else:
+            # Return empty string if the xml response is invalid
+            return {}
 
-        if element.tag == "authors":
-            author_list = []
-            for author in element:
-                for child in author:
-                    if child.tag == "name":
-                        author_list.append(child.text)
-                        bookDict["authors"]= (",").join(author_list)
+    # Exception for invalid url       
+    except requests.exceptions.RequestException:
+        raise Exception("InvalidGoodreadsURL")
+    
+    # Exception for xml ParseError
+    except ET.ParseError:
+        raise Exception("InvalidGoodreadsURL")
 
-    print(bookDict)
-
-    # Can be done using findall() method as well
-        # print(resp[1].findall('title')[0].text)
-        # print(resp[1].findall('average_rating')[0].text)
-        # print(resp[1].findall('ratings_count')[0].text)
-        # print(resp[1].findall('num_pages')[0].text)
-        # print(resp[1].findall('image_url')[0].text)
-        # print(resp[1].findall('publication_year')[0].text)
-        # # print(resp[1].findall('authors')[0].text)
-        # for author in resp[1].findall('authors')[0]:
-        #     print(author[1].text)
+    return bookDict
 
 # Prep url to use the api url format
-url = "https://www.goodreads.com/book/show/12177850-a-song-of-ice-and-fire" + ".xml?key="+ api_key
+url = "https://www.gooreads.com/book/show/12177850-a-song-of-ice-and-fire" + ".xml?key="+ api_key
 
-get_book_details(url)       
+print(get_book_details(url))       
